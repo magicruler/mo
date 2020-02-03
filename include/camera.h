62a7b4d5cbb20111f1fe933012d982035f2be5b4
Tianyu Li <ltyucb@gmail.com>
2020-02-02 19:46:25 -0700

    Fix ClearColor

--- include/camera.h
+++ include/camera.h
@@ -16,9 +16,9 @@ public:
 
 	float fov = 45.0f;
 	float ratio = 1024.0f / 768.0f;
-	float nearPlane = 0.1f;
-	float farPlane = 10000.0f;
+	float nearPlane = 0.3f;
+	float farPlane = 1000.0f;
 
-    glm::vec4 clearColor = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 clearColor = glm::vec4(49.0f/255.0f, 77.0f/255.0f, 121.0f/255.0f, 1.0f);
 	RenderTarget* renderTarget = nullptr;
 };
